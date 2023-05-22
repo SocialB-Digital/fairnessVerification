cf39122e0369500ae0f060db6efd7c9ac249856b
fida hussain <fida.socialb@gmail.com>
2023-05-22 19:44:28 +0500

    verification

--- src/Components/LimboVerification.jsx
+++ src/Components/LimboVerification.jsx
@@ -1,14 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import * as CryptoJS from "crypto-js";
 
 const LimboVerification = () => {
   const queryParams = new URLSearchParams(window.location.search);
-  const clientSeed = queryParams.get("clientseed");
-  const serverSeed = queryParams.get("serverseed");
-  const nonce = queryParams.get("nonce");
 
-  console.log("Here is the clientSeed ::: ", clientSeed);
-  console.log("Here is the server seed ::: ", serverSeed);
-  console.log("Here is the nonce ::: ", nonce);
+  const clientSeed = queryParams.get("clientSeed");
+  const serverSeed = queryParams.get("severSeed");
+  const nonce = queryParams.get("nonce");
+  const serverHashed = queryParams.get("serverSeedhashed");
+  const [clienthash, setClientHash] = useState(null);
+  const [limboResult, setlimboResult] = useState(null);
+  // http://localhost:3000/limbo/?clientSeed=clientSeed&severSeed=serverSeed&nonce=3
+  const result_hash = (serverSeed, clientSeed, nonce) => {
+    const message = clientSeed + ":" + nonce;
+    return CryptoJS.HmacSHA256(message, serverSeed).toString(CryptoJS.enc.Hex);
+  };
+  const gameResult = (result_hash_list, server_seed) => {
+    let seed = result_hash_list;
+    const nBits = 52;
+    seed = seed.slice(0, nBits / 4);
+    const r = parseInt(seed, 16);
+    let X = r / Math.pow(2, nBits);
+    X = 99 / (1 - X);
+    const result = Math.floor(X);
+    return server_seed ? Math.max(1, result / 100) : "";
+  };
+  useEffect(() => {
+    setlimboResult(
+      gameResult(result_hash(serverSeed, clientSeed, nonce), serverSeed)
+    );
+
+    setClientHash(result_hash(serverSeed, clientSeed, nonce));
+  }, []);
+
   return (
     <div id="app" class="main">
       <h1 class="text-center pb-5">Limbo verify</h1>
@@ -16,16 +40,21 @@ const LimboVerification = () => {
       <form class="py-5">
         <h2 class="text-center">Input</h2>
         <div class="form-group">
-          <input placeholder="Client Seed" class="form-control" />
+          <input
+            placeholder="Client Seed"
+            value={clientSeed}
+            class="form-control"
+          />
         </div>
         <div class="form-group">
-          <input placeholder="Server Seed" class="form-control" />
+          <input
+            placeholder="Server Seed"
+            value={serverSeed}
+            class="form-control"
+          />
         </div>
         <div class="form-group">
-          <input placeholder="Server Seed Hash" class="form-control" />
-        </div>
-        <div class="form-group">
-          <input placeholder="Nonce" class="form-control" />
+          <input placeholder="Nonce" value={nonce} class="form-control" />
         </div>
       </form>
       <hr />
@@ -33,11 +62,15 @@ const LimboVerification = () => {
         <h2 class="text-center pb-5">Output</h2>
         <div class="form-group">
           <label>sha256(server_seed)</label>
-          <input readonly="readonly" class="form-control" />
+          <input
+            readonly="readonly"
+            value={serverHashed}
+            class="form-control"
+          />
         </div>
         <div class="form-group">
           <label>hmac_sha256(client_seed:nonce, server_seed)</label>
-          <input readonly="readonly" class="form-control" />
+          <input readonly="readonly" value={clienthash} class="form-control" />
         </div>
       </form>
       <hr />
@@ -45,7 +78,7 @@ const LimboVerification = () => {
         <h2 class="text-center pb-5">Results</h2>
         <h5>Final Result</h5>
         <h5>hmac_sha256(client_seed:nonce, server_seed)</h5>
-        <input readonly="readonly" class="form-control" />
+        <input readonly="readonly" value={limboResult} class="form-control" />
       </form>
     </div>
   );
